5cb055f540447fc23ffb422ae1d70a89b4c65d6f
Wendrul <dethomassin.etienne@gmail.com>
2020-11-27 10:36:24 +0100

    Add bound and pins header

---
+++ include/bounds_pins.h
@@ -0,0 +1,27 @@
+#ifndef BOUNDS_PINS_H
+# define BOUNDS_PINS_H
+
+# define X_RIGHT_EYE_PIN 0
+# define Y_RIGHT_EYE_PIN 0
+# define X_LEFT_EYE_PIN 0
+# define Y_LEFT_EYE_PIN 0
+# define MOUTH_PIN 0
+# define ROLL_NECK_PIN 0
+# define YAW_NECK_PIN 0
+
+# define RIGHT_EYE_X_BOUND_MAX 180
+# define RIGHT_EYE_X_BOUND_MIN 0 
+# define RIGHT_EYE_Y_BOUND_MAX 180
+# define RIGHT_EYE_Y_BOUND_MIN 0
+# define LEFT_EYE_X_BOUND_MAX 180
+# define LEFT_EYE_X_BOUND_MIN 0
+# define LEFT_EYE_Y_BOUND_MAX 180
+# define LEFT_EYE_Y_BOUND_MIN 0
+# define MOUTH_BOUND_MAX 180
+# define MOUTH_BOUND_MIN 0
+# define ROLL_BOUND_MAX 180
+# define ROLL_BOUND_MIN 0
+# define YAW_BOUND_MAX 180
+# define YAW_BOUND_MIN 0
+
+#endif
