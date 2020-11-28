674cf116bc559fd5e49faf98d8f7a94889b4cf18
Wendrul <dethomassin.etienne@gmail.com>
2020-11-28 10:52:29 +0100

    Fix crazy servo (class is now a pointer)

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
                     X_LEFT_EYE_PIN, Y_LEFT_EYE_PIN,
                     MOUTH_PIN,
                     ROLL_NECK_PIN, YAW_NECK_PIN);*/
-RoboFace face;
+RoboFace *face;
 char buf[1024];
 int cur = 0;
 
@@ -16,25 +16,25 @@ void setup()
 {
     Serial.begin(9600);
 
-    face = RoboFace(X_RIGHT_EYE_PIN, Y_RIGHT_EYE_PIN, 
+    face = new RoboFace(X_RIGHT_EYE_PIN, Y_RIGHT_EYE_PIN, 
                     X_LEFT_EYE_PIN, Y_LEFT_EYE_PIN,
                     MOUTH_PIN,
                     ROLL_NECK_PIN, YAW_NECK_PIN);
-    face.rightEye.SetBoundaries(RIGHT_EYE_X_BOUND_MIN, RIGHT_EYE_X_BOUND_MAX,
+    face->rightEye.SetBoundaries(RIGHT_EYE_X_BOUND_MIN, RIGHT_EYE_X_BOUND_MAX,
                                 RIGHT_EYE_Y_BOUND_MIN, RIGHT_EYE_Y_BOUND_MAX);
-    face.leftEye.SetBoundaries(LEFT_EYE_X_BOUND_MIN, LEFT_EYE_Y_BOUND_MAX,
+    face->leftEye.SetBoundaries(LEFT_EYE_X_BOUND_MIN, LEFT_EYE_Y_BOUND_MAX,
                                 LEFT_EYE_Y_BOUND_MIN, LEFT_EYE_Y_BOUND_MAX);
-    face.mouth.SetBoundaries(MOUTH_BOUND_MIN, MOUTH_BOUND_MAX);
-    face.neck.SetBoundaries(ROLL_BOUND_MIN, ROLL_BOUND_MAX,
+    face->mouth.SetBoundaries(MOUTH_BOUND_MIN, MOUTH_BOUND_MAX);
+    face->neck.SetBoundaries(ROLL_BOUND_MIN, ROLL_BOUND_MAX,
                             YAW_BOUND_MIN, YAW_BOUND_MAX);   
 }
 
 void UpdatePos()
 {
-    face.rightEye.UpdatePos();
-    face.leftEye.UpdatePos();
-    face.mouth.UpdatePos();
-    face.neck.UpdatePos();
+    face->rightEye.UpdatePos();
+    face->leftEye.UpdatePos();
+    face->mouth.UpdatePos();
+    face->neck.UpdatePos();
 }
 
 void ClearBuf()
@@ -55,7 +55,7 @@ void loop()
         if (buf[cur - 1] == '\r')
         {
             Serial.println(buf);
-            face.ReadSerialInstruction(buf);
+            face->ReadSerialInstruction(buf);
             ClearBuf();
             UpdatePos();
         }
