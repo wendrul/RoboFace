3333788ac58e57d674e200085f6656edfa0fc6e1
Wendrul <dethomassin.etienne@gmail.com>
2020-11-29 21:58:52 +0100

    Updated bounds + color code pins

--- include/bounds_pins.h
+++ include/bounds_pins.h
@@ -1,22 +1,22 @@
 #ifndef BOUNDS_PINS_H
 # define BOUNDS_PINS_H
 
-# define X_RIGHT_EYE_PIN 3
-# define Y_RIGHT_EYE_PIN 5
-# define X_LEFT_EYE_PIN 6
-# define Y_LEFT_EYE_PIN 9
+# define X_RIGHT_EYE_PIN 3 //blue
+# define Y_RIGHT_EYE_PIN 5 //green
+# define X_LEFT_EYE_PIN 6 //orange
+# define Y_LEFT_EYE_PIN 9 //yellow
 # define MOUTH_PIN 10
 # define ROLL_NECK_PIN 11
 # define YAW_NECK_PIN 13
 
-# define RIGHT_EYE_X_BOUND_MIN 20
-# define RIGHT_EYE_X_BOUND_MAX 100
-# define RIGHT_EYE_Y_BOUND_MIN 75
-# define RIGHT_EYE_Y_BOUND_MAX 150
-# define LEFT_EYE_X_BOUND_MIN 55
-# define LEFT_EYE_X_BOUND_MAX 130
-# define LEFT_EYE_Y_BOUND_MIN 60
-# define LEFT_EYE_Y_BOUND_MAX 125
+# define RIGHT_EYE_X_BOUND_MIN 55
+# define RIGHT_EYE_X_BOUND_MAX 115
+# define RIGHT_EYE_Y_BOUND_MIN 63
+# define RIGHT_EYE_Y_BOUND_MAX 87
+# define LEFT_EYE_X_BOUND_MIN 47
+# define LEFT_EYE_X_BOUND_MAX 113
+# define LEFT_EYE_Y_BOUND_MIN 90
+# define LEFT_EYE_Y_BOUND_MAX 115
 # define MOUTH_BOUND_MIN 75
 # define MOUTH_BOUND_MAX 180
 # define ROLL_BOUND_MIN 70
